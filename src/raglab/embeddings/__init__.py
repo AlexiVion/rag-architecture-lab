1c4f6d800a8ca4ef0c7d5e4b6dd4316ffb0ee777
AlexiVion <89750150+AlexiVion@users.noreply.github.com>
2026-09-12 14:15:15 -0300

    feat: add embedding package exports

---
+++ src/raglab/embeddings/__init__.py
@@ -0,0 +1,4 @@
+from raglab.embeddings.base import EmbeddingProvider
+from raglab.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+
+__all__ = ["EmbeddingProvider", "SentenceTransformerEmbeddings"]
