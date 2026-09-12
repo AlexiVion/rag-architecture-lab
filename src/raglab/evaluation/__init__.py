75faf4ad19d5cc372d8334c80d1b42e88462ada6
AlexiVion <89750150+AlexiVion@users.noreply.github.com>
2026-09-12 14:19:12 -0300

    style: sort evaluation exports

--- src/raglab/evaluation/__init__.py
+++ src/raglab/evaluation/__init__.py
@@ -1,3 +1,3 @@
 from raglab.evaluation.metrics import ndcg_at_k, recall_at_k, reciprocal_rank
 
-__all__ = ["recall_at_k", "reciprocal_rank", "ndcg_at_k"]
+__all__ = ["ndcg_at_k", "recall_at_k", "reciprocal_rank"]
